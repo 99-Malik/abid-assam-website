41104a3cdfba75656aed1739ea29a93a832b338c
99-malik <usamaiftikhar718@gmail.com>
2025-12-27 19:20:13 +0300

    new chnages

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
         {children}
         <FixedCallButtons />
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16918976305"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17646485914"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config','AW-16918976305');
+            gtag('config','AW-17646485914');
           `}
         </Script>
       </body>
